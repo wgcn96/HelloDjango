93cba274242d0c805ed5a28c020707b8823efdc0
wangchen <wangchen@m.scnu.edu.cn>
2018-07-31 21:37:31 +0800

    add test module

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -10,8 +11,14 @@ class Question(models.Model):
     def __str__(self):   # 在python2版本中使用的是__unique__
         return self.question_text
 
+    '''
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    '''
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
